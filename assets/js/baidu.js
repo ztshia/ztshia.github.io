be4b9eb5740cbd79734735508cf68c3f9315e5b2
Kioto <ztshia@gmail.com>
2024-06-10 16:43:38 +0800

    Update baidu.js

--- assets/js/baidu.js
+++ assets/js/baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?00f864a3b605c9c1262e3c62a8c346cb";
+  hm.src = "https://hm.baidu.com/hm.js?1256b37c5645aecf91a6808907b5a23c";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
